8256a7bb66c04c4facbc73302f853a7bc0e2a799
Augusto Vicente <vicente96074@gmail.com>
2023-04-24 18:43:33 -0600

    Versión 1.0.0

--- src/main/java/com/softquark/gt/actualizableremoto/main/AppUpdater.java
+++ src/main/java/com/softquark/gt/actualizableremoto/main/AppUpdater.java
@@ -15,16 +15,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class AppUpdater {
-    
-    private static final String UPDATE_CONFIG_URL = "https://github.com/vicente96074/actualizableremoto/releases/latest/update/config.json";
-    private static final String APP_JAR_URL = "https://github.com/vicente96074/actualizableremoto/releases/latest/update/app.jar";
-    
+
+    private static final String UPDATE_CONFIG_URL = "https://github.com/vicente96074/actualizableremoto/tree/main/releases/latest/update/config.json";
+    private static final String APP_JAR_URL = "https://github.com/vicente96074/actualizableremoto/tree/main/releases/latest/update/app.jar";
+
     private static final String APP_JAR_FILE_NAME = "app.jar";
     private static final String APP_VERSION_FILE_NAME = "version.txt";
     private static final String APP_VERSION_PATTERN = "^\\d+\\.\\d+\\.\\d+$";
-    
+
     private static final Path APP_DIR = Paths.get(System.getProperty("user.dir"));
-    
+
     public static void main(String[] args) {
         try {
             String currentVersion = getCurrentVersion();
@@ -32,17 +32,18 @@ public class AppUpdater {
             if (latestVersion != null && !latestVersion.equals(currentVersion)) {
                 downloadUpdate();
                 restartApp();
+
+                System.out.println("¡Si hay versión nueva!");
+
             } else {
                 System.out.println("No updates available.");
             }
-            
-            
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     private static String getCurrentVersion() throws IOException {
         Path versionFile = APP_DIR.resolve(APP_VERSION_FILE_NAME);
         if (Files.exists(versionFile)) {
@@ -51,7 +52,7 @@ public class AppUpdater {
             return "0.0.0";
         }
     }
-    
+
     private static String getLatestVersion() throws IOException {
         URL updateConfigUrl = new URL(UPDATE_CONFIG_URL);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(updateConfigUrl.openStream()))) {
@@ -64,7 +65,7 @@ public class AppUpdater {
         }
         return null;
     }
-    
+
     private static void downloadUpdate() throws IOException {
         URL appJarUrl = new URL(APP_JAR_URL);
         Path appJarFile = APP_DIR.resolve(APP_JAR_FILE_NAME);
@@ -72,7 +73,7 @@ public class AppUpdater {
         Path versionFile = APP_DIR.resolve(APP_VERSION_FILE_NAME);
         Files.write(versionFile, getLatestVersion().getBytes());
     }
-    
+
     private static void restartApp() throws IOException {
         String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
         String classPath = System.getProperty("java.class.path");
