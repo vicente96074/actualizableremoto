affe937c44abc63c65ac6d1781e79ea2851ab653
Augusto Vicente <vicente96074@gmail.com>
2023-04-24 22:03:53 -0600

    Subir la versión 1.0.01

--- src/main/java/com/softquark/gt/actualizableremoto/main/MyApp.java
+++ src/main/java/com/softquark/gt/actualizableremoto/main/MyApp.java
@@ -5,22 +5,91 @@
  */
 package com.softquark.gt.actualizableremoto.main;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
 import java.io.IOException;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MyApp {
-
+    
     private final static String CURRENT_VERSION = "1.0.0";
-
+    
     public static void main(String[] args) {
+        
+        String estado = "";
+        
         try {
             if (UpdateChecker.isUpdateAvailable(CURRENT_VERSION)) {
                 System.out.println("¡Hay una actualización disponible!");
+                estado = "¡Hay una actualización disponible!";
                 // Mostrar un cuadro de diálogo al usuario para preguntar si desea actualizar
             } else {
                 System.out.println("¡Tu aplicación está actualizada!");
+                estado = "¡Tu aplicación está actualizada!";
             }
         } catch (IOException e) {
             System.err.println("Error al verificar actualizaciones: " + e.getMessage());
+            estado = "Error al verificar actualizaciones: " + e.getMessage();
         }
+        
+        new Ventana(estado).setVisible(true);
+        
     }
+}
+
+class Ventana extends JFrame {
+    
+    public Ventana(String estado) throws HeadlessException {
+        this.estado = estado;
+        initComponents();
+    }
+    
+    private void initComponents() {
+        this.setSize(new Dimension(300, 300));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        
+        this.setBackground(new Color(60, 63, 65));
+        
+        panel = new JPanel();
+        jLabel = new JLabel();
+        jButton = new JButton();
+        
+        jLabel.setText(estado);
+        jLabel.setHorizontalTextPosition(0);
+        
+        GroupLayout layoutPanel = new GroupLayout(panel);
+        panel.setLayout(layoutPanel);
+        layoutPanel.setHorizontalGroup(layoutPanel.createSequentialGroup()
+                .addGap(5, 5, Short.MAX_VALUE)
+                .addGroup(layoutPanel.createParallelGroup()
+                        .addComponent(jLabel, 200, 200, 200)
+                        .addComponent(jButton, 200, 200, 200)
+                )
+                .addGap(5, 5, Short.MAX_VALUE)
+        );
+        
+        layoutPanel.setVerticalGroup(layoutPanel.createSequentialGroup()
+                .addGap(5, 5, Short.MAX_VALUE)
+                .addComponent(jLabel, 30, 30, 30)
+                .addGap(5, 5, 5)
+                .addComponent(jButton, 30, 30, 30)
+                .addGap(5, 5, Short.MAX_VALUE)
+        );
+        
+        this.add(panel);
+        
+    }
+    
+    private String estado;
+    
+    private JPanel panel;
+    private JLabel jLabel;
+    private JButton jButton;
+    
 }
